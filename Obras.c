85d6324548e0f2dfecf1e159fc26d149011e7242
HvBarbosa <148903999+HvBarbosa@users.noreply.github.com>
2023-10-31 11:47:56 -0300

    Create Obras.c

---
+++ Obras.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+void imprimirCabecalho(){
+    puts("********************************************");
+    puts("*                                          *");
+    puts("*                 SysMuseu                 *");
+    puts("*                                          *");
+    puts("*                                ADS - UNIP*");
+    puts("********************************************");
+}
+
+void exibirMenu(){
+    puts("\nEscolha uma obra desejada:");
+    puts("1 - 150 anos de Santos Dumont");
+    puts("2 - 100 ANOS DA ARTE MODERNA");
+    puts("3 - INTELIGÊNCIA ARTIFICIAL (IA)");
+    puts("4 - JOGOS OLÍMPICOS 2024");
+    puts("5 - Todas as Obras");
+    puts("6 - Sair");
+    puts("_______________________________________________________");
+    printf("opc: ");
+}
+
+void limparBuffer(){
+    fflush(stdin);
+}
+
+void LimparTela(){
+    char ent;
+    puts("pressione qualquer tecla para continuar...");
+    fflush(stdin);
+    getchar();
+    system("cls");
+}
