fd204cae7ca4a41bda23a15104044a2e9a52e0ed
HvBarbosa <148903999+HvBarbosa@users.noreply.github.com>
2023-11-03 19:51:26 -0300

    Update Ingresso.h

--- Ingresso.h
+++ Ingresso.h
@@ -1,30 +1,24 @@
- typedef struct data Data;
+typedef struct data Data;
 
- struct data{
-
+struct data {
     int data;
     int mes;
     int ano;
 };
-    typedef struct data Data;
 
-    struct Ingresso{
-        char nome[20];
-        char obra[30];
-        int ticket;
-        int valor;
-        Data dtTicket;
-        float hora;
-    };
-    typedef struct ingresso Ingresso;
-    void printTipo(int tipo);
-    void comprarIngressos(struct ingresso Ingressos[], int numIngresso);
-    void valorDaCompra(int valor);
-    void printIngresso(Ingresso ingrs);
-    void printIngresso(ingresso ingressos[], int ticket);
-    void pagarIngresso(Ingresso ingressos[], int valorTotal);
-    void salvarDadosIngressos(Ingresso ingressos[], int ticket);
-    void carregarDadosIngressos(Ingresso ingressos[],int *pid);
+typedef struct Ingresso {
+    char nome[20];
+    char obra[30];
+    int ticket;
+    int valor;
+    Data dtTicket;
+    float hora;
+} Ingresso;
 
+void comprarIngressos(Ingresso Ingressos[]);
+void printIngresso(Ingresso ingrs);
+void printIngresso2(Ingresso ingressos[], int ticket);
+void salvarDadosIngressos(Ingresso ingressos[], int ticket);
+void carregarDadosIngressos(Ingresso ingressos[], int *pid);
 
 
